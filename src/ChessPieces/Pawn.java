529102016bb2b543517fc58915dc3e375104bd27
jack-e <139477720+jackychip@users.noreply.github.com>
2023-09-01 12:44:36 -0400

    fixed moving

--- src/ChessPieces/Pawn.java
+++ src/ChessPieces/Pawn.java
@@ -11,37 +11,61 @@ public class Pawn extends Piece {
     
     public Pawn(boolean white) {
         super(white);
-        direction = white ? 1 : -1;
+        direction = white ? -1 : 1;
         startingPos = white ? 6 : 1;
     }
 
     public boolean canMove(Board board, Square start, Square end) {
 
-        //x y delta coordinates 
-        int x = Math.abs(start.getY() - end.getY());
+        //y delta
         int y = start.getX() - end.getX();
+        int x = start.getY() - end.getY();
 
         //capturing
-        if (x == 1 && y == direction) {
-            //captured piece is there and not white
-            if (end.getPiece() != null && end.getPiece().isWhite() != this.isWhite()) {
-                return true;
-            }
+        if (canTake(start, end)) {
+            return true;
+        }
+
+        if (pieceInRoute(board, start, end)) {
+            System.out.println("e1");
             return false;
         }
 
         //moving
         if (start.getX() == startingPos) {
-            if (y == direction*2) {
+            if (y == -direction*2 && x == 0) {
                 return true;
             }
         }
         
-        if (y == direction && end.getPiece() == null) {
+        if (y == -direction && end.getPiece() == null && x == 0) {
             return true;
         }
 
         //TO-DO: en passant & promoting
+
+        return false;
+    }
+
+    public boolean pieceInRoute(Board board, Square start, Square end) {
+
+        //checking if a piece is blocking move
+        for(int i = 1; i < Math.abs(start.getX() - end.getX()); ++i){
+            if(board.getBox(start.getX() + i * direction, start.getY()).getPiece() != null) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean canTake(Square start, Square end) {
+        
+        if (Math.abs(start.getY() - end.getY()) == 1 && start.getX() - end.getX() == -direction) {
+            if (end.getPiece() != null && end.getPiece().isWhite() != this.isWhite()) {
+                return true;
+            }
+        }
 
         return false;
     }
